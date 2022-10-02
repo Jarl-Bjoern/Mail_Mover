6e9c003caee8825a30ee1a1e92d487467cb74943
Rainer Christian Björn Herold <106552800+Jarl-Bjoern@users.noreply.github.com>
2022-10-02 20:38:37 +0200

    Create HF.py

---
+++ HF.py
@@ -0,0 +1,9 @@
+try:
+    from os import mkdir, makedirs, remove, rename, system, walk
+    from os.path import exists, join
+    from re import finditer
+    from shutil import copy, SameFileError, SpecialFileError, ReadError, RegistryError, Error, ExecError
+    from sys import exit
+    from time import sleep
+except ModuleNotFoundError as e:
+    input(f"Das Modul {e} wurde nicht gefunden.\n\nBitte wenden Sie Sich an den Entwickler.\n\nZum Verlassen, bitte mit Enter bestaetigen.")
